2498430d71c3e2c4bd61e40d74d171120567433d
walkerNegrão <walker.esteves.negrao@gmail.com>
2022-06-23 20:11:16 -0300

    Adicionando a funcionalidade de depósito de valores na conta do user

--- index.mjs
+++ index.mjs
@@ -9,34 +9,46 @@ import fs from "fs"
 
 // console.log("Iniciando o Accounts....")
 
-/*Seção de funções: */
-function operation() { // Função que inicia o sistema de banco, oferendo as opções disponiveis.
+// Função que inicia o sistema de banco, oferendo as opções disponiveis:
+function operation() {
     inquirer.prompt([{
         type: "list",
         name: "action",
-        message: "O que você deseja fazer?",
+        message: "O que você deseja fazer?\n",
         choices: [
-            "Criar uma conta",
-            "Consultar o saldo",
-            "Realizar um depósito",
-            "Sair do sistema"
+            "Deseja criar uma conta",
+            "Deseja realizar um depósito",
+            "Deseja consultar o saldo",
+            "Deseja realizar um saque",
+            "Deseja sair do sistema"
         ],
     },
     ]).then((answer) => {
         const action = answer["action"] // Temos que usar a palavra reservada action para que seja exibida a resposta de escolha do user, isso porque foi definida em name acima
-        if (action === "Criar uma conta") {
+        if (action === "Deseja criar uma conta") {
             createAccount()
+        } else if (action === "Deseja realizar um depósito") {
+            deposit()
+        } else if (action === "Deseja consultar o saldo") {
+
+        } else if (action === "Deseja realizar um saque") {
+
+        } else if (action === "Deseja sair do sistema") {
+            console.log(chalk.bgBlue.red("sistema finalizado, obrigado por utilizar nosso sistema!!"))
+            process.exit() //Função que encerra o programa
         }
     }).catch((error) => { console.log(error) })
 }
 
-function createAccount() { //Função que da as boas vindas para o user
-    console.log(chalk.bgGreen.white("Obrigado por escolher o nosso banco!!"))
+//Função que da as boas vindas para o user:
+function createAccount() {
+    console.log(chalk.bgGreen.red("Obrigado por escolher o nosso banco!!\n"))
     console.log(chalk.yellow("Defina as opções da sua conta a seguir"))
     return buildAccount()
 
 }
 
+//Função que cria a conta do user:
 function buildAccount() {
     inquirer.prompt([{
         name: "accountName",
@@ -65,9 +77,74 @@ function buildAccount() {
     }).catch((error) => { console.log(error) })
 }
 
+//Função que realiza o depósito de um valor na conta do user:
+function deposit() {
+    inquirer.prompt([
+        {
+            name: "accountName",
+            message: "Qual o nome da conta?"
+        }
+    ]).then((answer) => {
+        const accountName = answer["accountName"]
+        //Chamando a função de verificação de conta existe:
+        if (!checkAccount(accountName)) { //Se a conta informa não existe a função retorna para o inicio da função deposit()
+            return deposit()
+        }
 
+        inquirer.prompt([
+            {
+                name: "amount",
+                message: "Qual o valor do depósito?"
+            }
+        ]).then((answer) => {
+            const amount = answer['amount']
+            addAmount(accountName, amount)
+            operation()
+        }).catch(error => console.log(error))
 
+    }).catch(error => console.log(error))
+}
 
+//Função que verifica se a conta informada existe:
+function checkAccount(accountName) {
+    if (!fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(chalk.bgRed.black("A conta informada não existe!!, informe uma conta registrada"))
+        return false
+    } else {
+        return true
+    }
+}
+
+//Função que seleciona a conta(arquivo):
+function getAccount(accountName) {
+    const accountJSON = fs.readFileSync(`accounts/${accountName}.json`, { //Lendo o arquivo da conta
+        encoding: "utf-8", //Colocando o padrão de escrita com acento
+        flag: "r" //Informando que queremos apenas ler
+    })
+
+    return JSON.parse(accountJSON)
+}
+
+//Função que adiciona valores na conta do user:
+function addAmount(accountName, amount) {
+    const accountData = getAccount(accountName)
+
+    if (!amount) {
+        console.log(chalk.bgRed.black("Ocorreu um erro, tente novamente!!"))
+        return deposit()
+    }
+
+    accountData.balance = parseFloat(amount) + parseFloat(accountData.balance) // Transformando o valor de string para number e somando o valor ao valor já armazenado na conta
+    fs.writeFileSync( // Estou escrendo no arquivo da conta o valor do depósito
+        `accounts/${accountName}.json`,
+        JSON.stringify(accountData),
+        function (error) {
+            console.log(error)
+        }
+    )
+
+    console.log(chalk.green(`Foi depositado o valor de RS${amount} na sua conta`))
+}
 
 
 
